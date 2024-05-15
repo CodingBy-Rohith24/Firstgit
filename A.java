d3f8e9f07d93813ab336bd5805dbcfb2b9295aed
CodingBy-Rohith24 <rohithbalina@gmail.com>
2024-05-15 20:40:13 +0530

    commiting A and Ar9

---
+++ A.java
@@ -0,0 +1,11 @@
+class A
+{
+int i=1;
+int j=2;
+void funA()
+{
+System.out.println(i);
+System.out.println(j);
+
+}
+}
---
+++ Ar9.java
@@ -0,0 +1,12 @@
+class Ar9
+{
+public static void main(String[] args)
+{
+int a=1;
+int b=a++;//b=1;a=2
+System.out.println("b:"+b);
+int i=1;
+int s=++i;//s=2;i=2
+System.out.println("s:"+s);
+}
+}
